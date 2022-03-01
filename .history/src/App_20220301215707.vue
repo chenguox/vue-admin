846a7b6c4b7e5831333246ead724b565080bbd52
陈国鑫 <529488618@qq.com>
2022-03-01 23:33:53 +0800

    fix: 项目启动失败修复

---
+++ .history/src/App_20220301215707.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">首页</router-link>
+    <router-view></router-view>
+
+    <h2>{{ this.$store.state.name }}</h2>
+
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import { ElButton } from 'element-plus'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ElButton
+  }
+})
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
